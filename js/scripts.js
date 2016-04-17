65581de34304114c94b0b703ce88601454ea94fa
rilsta <rstarnes9@gmail.com>
2016-04-17 13:31:58 -0700

    add hide/show jquery to sublist items

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-     $('.parallax').parallax();
-   });
+  $('#dentistry-list-item').click(function(){
+    $('#dentistry-sub-list').slideToggle();
+  });
+  $('#laboratory-list-item').click(function(){
+    $('#laboratory-sub-list').slideToggle();
+  });
+  $('#surgery-list-item').click(function(){
+    $('#surgery-sub-list').slideToggle();
+  });
+  $('#fertility-list-item').click(function(){
+    $('#fertility-sub-list').slideToggle();
+  });
+  $('#internal-list-item').click(function(){
+    $('#internal-sub-list').slideToggle();
+  });
+});
